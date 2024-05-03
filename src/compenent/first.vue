92cdaef5e999093d7a971582aaf6835b905394c7
Dtnks <3070228868@qq.com>
2024-05-03 21:13:27 +0800

    试一试

--- src/compenent/first.vue
+++ src/compenent/first.vue
@@ -2,15 +2,7 @@
 
 </template>
 <script lang="js" setup name="first">
-    import {ref} from 'vue'
-    let num=ref(3324)
-    let user='viuv'
-    function onclick() {
-        name="zhangsan"
-    }
-    function name(params) {
-        
-    }
+
 </script>
 
 <style>
